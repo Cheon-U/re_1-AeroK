47c47da3bc8e6d3a43bddae9414ed630485c19d8
amesystand <amesystand@gmail.com>
2025-10-14 18:35:17 +0900

    닫기버튼 추가

--- js/01_reserve_popup.js
+++ js/01_reserve_popup.js
@@ -3,7 +3,7 @@ document.getElementById('popup-search-start').addEventListener('click', function
   window.open(
     'popup-search-start.html',           // 팝업 파일 경로
     'LocationPopupStart',       // 팝업 이름 (중복 방지용)
-    'width=800,height=640'  // 팝업 크기
+    'width=800,height=700'  // 팝업 크기
   );
 });
 
@@ -13,7 +13,7 @@ document.getElementById('popup-search-arrived').addEventListener('click', functi
   window.open(
     'popup-search-arrived.html',           // 팝업 파일 경로
     'LocationPopupArrived',       // 팝업 이름 (중복 방지용)
-    'width=800,height=640'  // 팝업 크기
+    'width=800,height=700'  // 팝업 크기
   );
 });
 
@@ -23,16 +23,16 @@ document.getElementById('popup-calendar-start').addEventListener('click', functi
   window.open(
     'popup-calender-start.html',           // 팝업 파일 경로
     'calendarPopupStart',       // 팝업 이름 (중복 방지용)
-    'width=760,height=460'  // 팝업 크기
+    'width=840,height=530'  // 팝업 크기
   );
 });
 
-// 팝업-도착 날짜
+// 팝업-도착날짜
 document.getElementById('popup-calendar-end').addEventListener('click', function () {
   window.open(
     'popup-calender-end.html',           // 팝업 파일 경로
     'calendarPopupEnd',       // 팝업 이름 (중복 방지용)
-    'width=760,height=460'  // 팝업 크기
+    'width=840,height=530'  // 팝업 크기
   );
 });
 
@@ -51,6 +51,6 @@ document.getElementById('popup-select-seat').addEventListener('click', function 
   window.open(
     'popup-select-seat.html',           // 팝업 파일 경로
     'seatoPopup',       // 팝업 이름 (중복 방지용)
-    'width=1000,height=240'  // 팝업 크기
+    'width=1000,height=300'  // 팝업 크기
   );
 });
---
+++ js/popup-select-seat.js
@@ -0,0 +1,5 @@
+// 팝업창 닫기
+const closeBtn = document.querySelector(".btn-close img");
+closeBtn.addEventListener("click", () => {
+    window.close();
+});
